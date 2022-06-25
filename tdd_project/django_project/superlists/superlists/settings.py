28b40f0c1f6ac7c40fb4e3d5b416762d9920b8bf
SUNGJIN <kiata@naver.com>
2022-06-26 02:44:02 +0900

    commit (secret_key)

--- tdd_project/django_project/superlists/superlists/settings.py
+++ tdd_project/django_project/superlists/superlists/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5l1@4vri5lk=ie^+uqi6!!$we&ajlr-69duh4zq+oaytb*pu$w'
+SECRET_KEY = os.path.join(BASE_DIR, 'secrets.json')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

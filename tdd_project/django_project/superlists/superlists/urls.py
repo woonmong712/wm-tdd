9c026498889db5acc77d41e4460cbd940813cfe3
woonmong <kiata@naver.com>
2022-06-26 15:30:17 +0900

    3장 첫 단위 테스트와 url 맵핑 그리고 임시 뷰

--- tdd_project/django_project/superlists/superlists/urls.py
+++ tdd_project/django_project/superlists/superlists/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from lists import views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Examples : 
+    path('', views.home_page, name='home'),
+    # path('^blog/',include('blog.urls')),
+    # path('^admin/', include(admin.site.urls)),
 ]

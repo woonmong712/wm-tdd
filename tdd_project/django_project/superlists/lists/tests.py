d3b9e43951972da1609ff177a2592b054a685887
SUNGJIN <kiata@naver.com>
2022-07-04 01:47:04 +0900

    commit

--- tdd_project/django_project/superlists/lists/tests.py
+++ tdd_project/django_project/superlists/lists/tests.py
@@ -15,9 +15,22 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-    
+
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertTrue(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
--- tdd_project/django_project/superlists/lists/views.py
+++ tdd_project/django_project/superlists/lists/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 # home_page = None
 
+
 def home_page(request):
-    return render(request, 'home.html')
+    # if request.method == 'POST':
+    #     return HttpResponse(request.POST['item_text'])
+    return render(request, 'home.html', {
+        'new_itme_text': request.POST.get('item_text', ''),
+    })

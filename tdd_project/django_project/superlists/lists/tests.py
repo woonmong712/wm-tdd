863974a2ea6fb765635b851a28306d58ea814f21
woonmong <kiata@naver.com>
2022-07-09 12:34:19 +0900

    POST 후에 리디렉션과 템플릿에 모든 작업 아이템 표시

--- tdd_project/django_project/superlists/lists/tests.py
+++ tdd_project/django_project/superlists/lists/tests.py
@@ -5,6 +5,7 @@ from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 import re
+from lists.models import Item
 
 
 # Create your tests here.
@@ -25,6 +26,9 @@ class HomePageTest(TestCase):
             self.remove_csrf(html_code2)
         )
 
+    def assertInExceptCSRF(self, html_code1,html_code2):
+        return self.assertIn(self.remove_csrf(html_code1),self.remove_csrf(html_code2))
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -41,9 +45,63 @@ class HomePageTest(TestCase):
         request.POST['item_text'] = '신규 작업 아이템'
 
         response = home_page(request)
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': '신규 작업 아이템'}
-        )
-        self.assertEqualExceptCSRF(response.content.decode(),expected_html)
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        # expected_html = render_to_string(
+        #     'home.html',
+        #     {'new_item_text': '신규 작업 아이템'}
+        # )
+        self.assertEqual(new_item.text,'신규 작업 아이템')
+        # self.assertEqualExceptCSRF(response.content.decode(),expected_html)
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(response['location'],'/')
+
+        # self.assertIn('신규 작업 아이템', response.content.decode())
+        # expected_html = render_to_string(
+        #     'home.html',
+        #     {'new_item_text': '신규 작업 아이템'}
+        # )
+        # self.assertEqualExceptCSRF(response.content.decode(), expected_html)
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'],'/')
+
+    def test_home_page_only_saves_item_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(),0)
+
+    def test_home_page_displays_all_list_times(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+        
+        self.assertInExceptCSRF('itemey 1', response.content.decode())
+        self.assertInExceptCSRF('itemey 2', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text,'두 번째 아이템')
--- tdd_project/django_project/superlists/lists/views.py
+++ tdd_project/django_project/superlists/lists/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect,render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 # home_page = None
 
 
 def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request,'home.html',{'items' : items})
+
+    # item = Item()
+    # item.text = request.POST.get('item_text','')
+    # item.save()
     # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
+    #     new_item_text = request.POST['item_text']
+    #     Item.objects.create(text=new_item_text)
+    # else:
+    #     new_item_text = ''
     # return render(request, 'home.html',{
     #     'new_item_text' : request.POST['item_text'],
     # })
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    # return render(request, 'home.html', {
+    #     'new_item_text': request.POST.get('item_text', ''),
+    # })
+    # return render(request, 'home.html', {
+    #     'new_item_text': new_item_text,
+    # })
     # return render(request,'home.html')

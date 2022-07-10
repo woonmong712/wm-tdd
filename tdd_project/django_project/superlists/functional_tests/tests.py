579a5257b48d73fa66fd44c164d743b104ba5aa9
woonmong <kiata@naver.com>
2022-07-10 13:18:03 +0900

    새로운 모델과 뷰 생성

--- tdd_project/django_project/superlists/functional_tests/tests.py
+++ tdd_project/django_project/superlists/functional_tests/tests.py
@@ -2,12 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions
 import time
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -24,7 +23,8 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다.
-        self.browser.get('http://localhost:8000')
+        # self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
         self.assertIn('To-Do', self.browser.title)
@@ -49,6 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         # "1: 공작깃털 사기" 아이템이 추가된다
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # table = self.browser.find_element(By.ID, 'id_list_table')
@@ -72,24 +74,34 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         self.check_for_row_in_list_table('1: 공작깃털 사기')
         self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
-        # time.sleep(1)
+        
+        # 새로운 사용자인 프란시스가 사이트에 접속한다.
+        ## 새로운 브라우저 세션을 이용해서 에디스의 정보가
+        ## 쿠키를 통해 유입되는 것을 방지한다.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-        # time.sleep(1)
-        # table = self.browser.find_element(By.ID, 'id_list_table')
-        # rows = table.find_elements(By.TAG_NAME,'tr')
-        # self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        # self.assertIn(
-        #     '2: 공작깃털을 이용해서 그물 만들기',
-        #     [row.text for row in rows]
-        # )
-        # 에디스는 사이트가 입력한 목록을 저장하고 있는지 구감하다
-        # 사이트는 그녀를 위한 특정 URL을 생성해준다
-        # 이때 URL에 대한 설명도 함께 제공된다
+        # 프란시스가 홈페이지에 접속한다
+        # 에디스의 리스트는 보이지 않는다
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('공작깃털 사기',page_text)
+        self.assertNotIn('그물 만들기',page_text)
 
-        # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
-        self.fail('Finish the test!')
-        # 만족하고 잠자리에 든다.
+        # 프란시스가 새로운 작업 아이템을 입력하기 시작한다
+        # 그는 에디스보다 재미가 없다
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('우유 사기')
+        inputbox.send_keys(Keys.ENTER)
 
+        # 프란시스가 전용 URL을 취득한다.
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+        # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다.
+        page_text = self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('공작깃털 사기',page_text)
+        self.assertIn('우유 사기',page_text)
+
+        # 둘 다 만족하고 잠자리에 든다.

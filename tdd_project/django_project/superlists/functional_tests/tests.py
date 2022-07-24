2e0db6d021186f05cbca13d5560fa8e1f9e998e9
woonmong <kiata@naver.com>
2022-07-24 17:53:35 +0900

    레이아웃과 스타일링을 위한 FT 첫 단계

--- tdd_project/django_project/superlists/functional_tests/tests.py
+++ tdd_project/django_project/superlists/functional_tests/tests.py
@@ -108,3 +108,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('우유 사기', page_text)
 
         # 둘 다 만족하고 잠자리에 든다.
+
+    def test_layout_and_styling(self):
+        #에디스는 메인 페이지를 방문한다.
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_position(1024,768)
+
+        # 그녀는 입력 상자가 가운데 배치된 것을 본다
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
